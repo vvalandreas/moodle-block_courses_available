2ea5ee66f120dc93b6608b000279456a713e30c9
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-04-25 08:32:20 +0100

    First commit.

---
+++ lang/en/block_courses_available.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Courses Available block English language translation
+ *
+ * @package    block_courses_available
+ * @copyright  2017 Ian Wild
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configtitle'] = 'Courses taken';
+$string['pluginname'] = 'Courses available';
+$string['sfhcoursestaken'] = 'Courses started/completed';
+$string['fullnamedisplay'] = 'Courses started';
+$string['coursename'] = 'Name';
+$string['status'] = 'Status';
+$string['notstarted'] = 'Not started';
+$string['coursestatus'] = 'Course status';
+$string['complete'] = 'Complete';
+$string['inprogress'] = 'In progress';
+$string['notstarted'] = 'Not started';
+$string['score'] = 'Score';
+$string['viewprogress'] = 'View progress';
+$string['viewcertificates'] = 'View certificates';
+$string['completedattempts'] = 'Attempts completed';
+$string['laststudydate'] = 'Last study date';
+$string['continuecourse'] = 'Continue course';
+$string['retakecourse'] = 'Start course again';
+$string['description'] = 'Description';
+$string['completed'] = 'completed';
+$string['viewlearnersprogress'] = 'View progress';
